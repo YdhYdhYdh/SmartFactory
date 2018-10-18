a9c27a01e5edbe7d8e96a80f13f4c896697062f9
Yoo <yhm9207@naver.com>
2018-10-18 11:09:44 +0900

    V1

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_20180912
+{
+    class Program
+    {
+        static void Print(string name, string phone)
+        {
+            WriteLine($"이름 : {name}, 번호 : {phone}");
+        }
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 4; ++i)
+                WriteLine(i + " Hello");
+
+            Print(name: "유형민", phone: "010-4489-9371");
+
+            InitIronMan ArmorSuite = new InitIronMan();
+            ArmorSuite.Initialize();
+
+            InitIronMan IronMan = new IronMan();
+            IronMan.Initialize();
+
+            InitIronMan ArmorSuite2 = new ArmorSuite();
+            ArmorSuite2.Initialize();
+        }
+
+    }
+
+    class InitIronMan
+    {
+        public virtual void Initialize()
+        {
+            Console.WriteLine("Armor Suite");
+        }
+    }
+}
